650e59933396f81a4cb1912c9d116e47ccd43228
VolodymyrSF <139223674+VolodymyrSF@users.noreply.github.com>
2023-10-17 15:05:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {Simpsons} from "./components/simpsons";
-import {RickAndMortyApi} from "./components/RickAndMortyApi";
 const App = () => {
     return (
         <div>
-            <h2>--------------------- TASK WITH SIMPSONS -----------------</h2>
-            <Simpsons/>
-            <h2>--------------------- TASK WITH RICK -----------------------</h2>
-            <RickAndMortyApi/>
+           
         </div>
     );
 };
