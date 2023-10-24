c56080c7d55fe3f2c3d90cc095eec5b23b18d1dc
VSF <safjanik.volody@gmail.com>
2023-10-24 18:37:21 +0300

    add reset to form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,11 +3,12 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import {userService} from "../services/userService";
 const UserForm = ({setUsers}) => {
-    const {handleSubmit,register}=useForm()
+    const {reset,handleSubmit,register}=useForm()
 
     const save=async (user)=>{
         const {data}=await userService.create(user)
         setUsers(prev=>[...prev,data])
+        reset()
     }
     return (
         <form onSubmit={handleSubmit(save)}>
